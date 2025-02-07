50c7db88d0afe6d8adae2b491f7214fbad337198
quveryFish <yaromdem@gmail.com>
2025-02-07 18:43:46 +0200

    Improved Map, Done

--- Assets/ProjectFiles/Scripts/Road/RoadManage/RoadRemover.cs
+++ Assets/ProjectFiles/Scripts/Road/RoadManage/RoadRemover.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 public class RoadRemover : MonoBehaviour
 {
     [SerializeField] Transform spawner;
+    [SerializeField] RoadSpawner RoadSpawner;
+    private void Start()
+    {
+        
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        collision.gameObject.transform.position = spawner.position;
+        //collision.gameObject.transform.position = spawner.position;
+        Destroy(collision.gameObject);
+        RoadSpawner.roads.Remove(collision.gameObject);
     }
 }
