066c4328f751d950a0f3a4232c55be775b231b98
Krypton1397 <andreyusok2007@gmail.com>
2025-02-10 17:24:42 +0200

    AddedGameUiMenu

---
+++ Assets/ProjectFiles/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject PauseWindow;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0;
+            PauseWindow.SetActive(true);
+        }
+    }
+    public void RestartScreen()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void ContinueScreen()
+    {
+        Time.timeScale = 1f;
+        PauseWindow.SetActive(false);
+    }
+}
