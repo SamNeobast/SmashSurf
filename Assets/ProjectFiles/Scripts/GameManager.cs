b4e205ed0a01c9d791df8a108443795f7594bca6
Krypton1397 <andreyusok2007@gmail.com>
2025-03-16 23:03:10 +0200

    AddedSoundsBackgrounds

--- Assets/ProjectFiles/Scripts/GameManager.cs
+++ Assets/ProjectFiles/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (LoseWindow == false)
+        if (LoseWindow.activeSelf == false)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
